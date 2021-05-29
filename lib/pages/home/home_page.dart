881be9bb371c38e48866c818b5855cdb31752593
FELIPE FREIRE <felipe.freire@intuitivecare.com>
2021-05-29 16:11:22 -0300

    circular

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -42,10 +42,11 @@ class _MyHomePageState extends State<MyHomePage> {
 
   getBairro() async {
     bairroList = await api.getBairro();
-
+    loading = true;
     setState(() {});
   }
 
+  bool loading = false;
   String selectUF = '';
   String selectCidade = '';
   String selectBairro = '';
@@ -175,75 +176,77 @@ class _MyHomePageState extends State<MyHomePage> {
             ),
             Container(
               padding: EdgeInsets.symmetric(horizontal: 30),
-              child: Column(
-                children: [
-                  DropdownSearch<String>(
-                    mode: Mode.MENU,
-                    showSearchBox: true,
-                    dropdownSearchDecoration: InputDecoration(
-                      border: OutlineInputBorder(),
-                      focusedBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Color(0xff4FB7D4),
-                              style: BorderStyle.solid,
-                              width: 2)),
-                      enabledBorder: OutlineInputBorder(
-                          borderSide: BorderSide(
-                              color: Color(0xff4FB7D4),
-                              style: BorderStyle.solid,
-                              width: 2)),
-                      contentPadding: EdgeInsets.fromLTRB(12, 0, 0, 0),
-                      labelStyle:
-                          TextStyle(fontSize: 20, color: Color(0xff4FB7D4)),
-                    ),
-                    items: bairroList.map((e) => e.bairro).toList(),
-                    label: "Bairro",
-                    hint: "country in menu mode",
-                    // popupItemDisabled: (String s) => s.startsWith('I'),
-                    onChanged: (data) {
-                      setState(() {
-                        selectBairro = data;
-                      });
-                    },
-                    selectedItem: selectBairro == ''
-                        ? "Selecione o bairro"
-                        : selectBairro,
-                  ),
-
-                  // button
-                  SizedBox(
-                    height: 10,
-                  ),
-                  Container(
-                    height: 40,
-                    width: double.infinity,
-                    child: TextButton(
-                      style: TextButton.styleFrom(
-                          backgroundColor: Color(0xff3AB2D6)),
-                      child: Text(
-                        "Buscar",
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 16.5,
-                        ),
-                      ),
-                      onPressed: () async {
-                        print(api);
-                        var result = await api.getUnidade(selectBairro);
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => ResultsPage(
-                              unidades: result,
-                              api: api,
-                            ),
+              child: loading == false
+                  ? CircularProgressIndicator()
+                  : Column(
+                      children: [
+                        DropdownSearch<String>(
+                          mode: Mode.MENU,
+                          showSearchBox: true,
+                          dropdownSearchDecoration: InputDecoration(
+                            border: OutlineInputBorder(),
+                            focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xff4FB7D4),
+                                    style: BorderStyle.solid,
+                                    width: 2)),
+                            enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Color(0xff4FB7D4),
+                                    style: BorderStyle.solid,
+                                    width: 2)),
+                            contentPadding: EdgeInsets.fromLTRB(12, 0, 0, 0),
+                            labelStyle: TextStyle(
+                                fontSize: 20, color: Color(0xff4FB7D4)),
                           ),
-                        );
-                      },
+                          items: bairroList.map((e) => e.bairro).toList(),
+                          label: "Bairro",
+                          hint: "country in menu mode",
+                          // popupItemDisabled: (String s) => s.startsWith('I'),
+                          onChanged: (data) {
+                            setState(() {
+                              selectBairro = data;
+                            });
+                          },
+                          selectedItem: selectBairro == ''
+                              ? "Selecione o bairro"
+                              : selectBairro,
+                        ),
+
+                        // button
+                        SizedBox(
+                          height: 10,
+                        ),
+                        Container(
+                          height: 40,
+                          width: double.infinity,
+                          child: TextButton(
+                            style: TextButton.styleFrom(
+                                backgroundColor: Color(0xff3AB2D6)),
+                            child: Text(
+                              "Buscar",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 16.5,
+                              ),
+                            ),
+                            onPressed: () async {
+                              print(api);
+                              var result = await api.getUnidade(selectBairro);
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => ResultsPage(
+                                    unidades: result,
+                                    api: api,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        ),
+                      ],
                     ),
-                  ),
-                ],
-              ),
             ),
           ],
         ),
